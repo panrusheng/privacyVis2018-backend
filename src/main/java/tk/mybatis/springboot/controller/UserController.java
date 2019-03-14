f52d4a228e1fc6a8ee70223e26a29ba8e71b359a
panrusheng <panrusheng@zju.edu.cn>
2019-03-14 09:55:59 +0800

    default dataset user

--- src/main/java/tk/mybatis/springboot/controller/UserController.java
+++ src/main/java/tk/mybatis/springboot/controller/UserController.java
@@ -25,7 +25,7 @@ public class UserController {
     private Bayes bn;
 
     UserController() {
-        bn = new Bayes("student");
+        bn = new Bayes("user");
     }
 
     @RequestMapping//Home
