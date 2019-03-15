a90c70239b35d723231dcf6e948aa28aec466867
kotta <3150104312@zju.edu.cn>
2019-03-15 23:35:32 +0800

    bugfix

--- src/main/java/tk/mybatis/springboot/util/algorithm/Bayes.java
+++ src/main/java/tk/mybatis/springboot/util/algorithm/Bayes.java
@@ -1310,9 +1310,9 @@ public class Bayes {
 
             /* split the numeric data */
             List<String> attributeValues = new ArrayList<>();
-            attributeValues.add("[" + integerTrimEndZero(df.format(minValue)) + "~" + integerTrimEndZero(df.format(minValue + splitPoint.get(0))) + "]");
+            attributeValues.add("[" + integerTrimEndZero(df.format(minValue)) + "~" + integerTrimEndZero(df.format(splitPoint.get(0))) + "]");
             for(int i = 0, size = splitPoint.size()-1; i < size; i++){
-                String category = "(" + integerTrimEndZero(df.format(minValue + splitPoint.get(i))) + "~" + integerTrimEndZero(df.format(minValue + splitPoint.get(i+1))) + "]";
+                String category = "(" + integerTrimEndZero(df.format(minValue + splitPoint.get(i))) + "~" + integerTrimEndZero(df.format(splitPoint.get(i+1))) + "]";
                 attributeValues.add(category);
             }
             attributeValues.add("(" + integerTrimEndZero(df.format(minValue + splitPoint.get(splitPoint.size()-1))) + "~" + integerTrimEndZero(df.format(maxValue)) + "]");
