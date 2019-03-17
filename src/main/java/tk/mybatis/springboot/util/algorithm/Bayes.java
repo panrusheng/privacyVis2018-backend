672f98f326815979d4141066308c9da316e886e2
kotta <3150104312@zju.edu.cn>
2019-03-17 21:13:05 +0800

    fix

--- src/main/java/tk/mybatis/springboot/util/algorithm/Bayes.java
+++ src/main/java/tk/mybatis/springboot/util/algorithm/Bayes.java
@@ -568,14 +568,23 @@ public class Bayes {
                     double minRate = Double.POSITIVE_INFINITY;
                     List<Integer> oriGroupList = this.attGroupList.get(attName).getT0();
                     int[] curGroupList = numericEventCntMap.get(attName);
-                    Enumeration e = this.data.attribute(attName).enumerateValues();
-                    while(e.hasMoreElements()){
-                        String category = (String)e.nextElement();
-                        String eventName = attName+": "+category;
-                        int oriV = eventCntMap.get(eventName).get(0);
-                        int curV = eventCntMap.get(eventName).get(eventCntMap.get(eventName).size()-1);
+//                    Enumeration e = this.data.attribute(attName).enumerateValues();
+//                    while(e.hasMoreElements()){
+//                        String category = (String)e.nextElement();
+//                        String eventName = attName+": "+category;
+//                        int oriV = eventCntMap.get(eventName).get(0);
+//                        int curV = eventCntMap.get(eventName).get(eventCntMap.get(eventName).size()-1);
+//                        double rate = (double)curV / oriV;
+//                        if(rate < minRate){
+//                            minRate = rate;
+//                        }
+//                    }
+                    for (int i = 0; i < oriGroupList.size(); ++i) {
+                        int oriV = oriGroupList.get(i);
+                        int curV = curGroupList[i];
+                        if (oriV == 0) continue;
                         double rate = (double)curV / oriV;
-                        if(rate < minRate){
+                        if (rate < minRate) {
                             minRate = rate;
                         }
                     }
